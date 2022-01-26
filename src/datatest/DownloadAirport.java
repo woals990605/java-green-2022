ebc8843cc8581a1e8a1a7374897d40ff978bddfe
woals990605 <woals990605@naver.com>
2022-01-26 12:30:53 +0900

    공공데티어 쪽지시험까지

--- src/datatest/DownloadAirport.java
+++ src/datatest/DownloadAirport.java
@@ -1,6 +1,8 @@
 package datatest;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -15,12 +17,15 @@ import datatest.AirportDto.Response.Body.Items.Item;
 
 public class DownloadAirport {
     public static Map<String, String> getAirportList() {
-        Map<String, String> airpoMap = new HashMap<>();
+        Map<String, String> airportMap = new HashMap<>();
 
+        URL url;
         try {
-            URL url = new URL(
+            url = new URL(
                     "http://openapi.tago.go.kr/openapi/service/DmstcFlightNvgInfoService/getArprtList?serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&_type=json");
+
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(conn.getInputStream(), "utf-8"));
 
@@ -30,12 +35,11 @@ public class DownloadAirport {
             List<Item> result = dto.getResponse().getBody().getItems().getItem();
 
             for (int i = 0; i < result.size(); i++) {
-                airpoMap.put(result.get(i).getAirportNm(), result.get(i).getAirportId());
+                airportMap.put(result.get(i).getAirportNm(), result.get(i).getAirportId());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("공항목록 조회중 오류가 발생했습니다.");
         }
-        return airpoMap;
-
+        return airportMap;
     }
 }
